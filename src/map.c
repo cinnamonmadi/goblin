6fbde3ab32232c6f6dbb13a50c4749a734f66789
Madi Madden <cinnamon.madi@gmail.com>
2021-07-14 13:20:13 -0500

    Map generates walls instead of floor tiles
    Signed-off-by: Madi Madden <cinnamon.madi@gmail.com>

--- src/map.c
+++ src/map.c
@@ -9,11 +9,20 @@ Map* map_init(int width, int height, MapParams params){
     map->width = width;
     map->height = height;
 
-    bool success = false;
-    while(!success){
+    RoomData* data;
+    bool data_successful = false;
+    while(!data_successful){
 
-        map->tiles = map_generate_tiles(width, height, params, &success);
+        data = map_room_data_generate(width, height, params);
+        data_successful = map_room_data_validate(data, params);
+        if(!data_successful){
+
+            map_room_data_free(data);
+        }
     }
+    map->tiles = map_generate_tiles(width, height, data);
+
+    map_room_data_free(data);
 
     return map;
 }
@@ -28,46 +37,29 @@ void map_free(Map* map){
     free(map);
 }
 
-void create_room(Vector** tiles, SDL_Rect* room){
-
-    for(int x = room->x; x < room->x + room->w; x++){
-
-        for(int y = room->y; y < room->y + room->h; y++){
-
-            tiles[x][y] = SPRITE_TILE_WALL;
-        }
-    }
-}
-
-Vector** map_generate_tiles(int width, int height, MapParams params, bool* success){
-
-    Vector** tiles = malloc(sizeof(Vector*) * width);
-    for(int x = 0; x < width; x++){
-
-        tiles[x] = malloc(sizeof(Vector) * height);
-        for(int y = 0; y < height; y++){
-
-            tiles[x][y] = SPRITE_NONE;
-        }
-    }
+RoomData* map_room_data_generate(int width, int height, MapParams params){
 
     // Generate rooms
-    SDL_Rect rooms[params.max_rooms];
-    int room_count = 0;
+    RoomData* data = malloc(sizeof(RoomData));
+    *data = (RoomData){
+        .rooms = malloc(sizeof(SDL_Rect) * params.max_rooms),
+        .hallways = malloc(sizeof(SDL_Rect) * (params.max_rooms - 1) * 2),
+        .room_count = 0
+    };
     for(int r = 0; r < params.max_rooms; r++){
 
         int w = rand_range(params.room_min_size, params.room_max_size);
         int h = rand_range(params.room_min_size, params.room_max_size);
-        int x = rand_range(0, width - w - 1);
-        int y = rand_range(0, height - h - 1);
+        int x = rand_range(1, width - w - 1);
+        int y = rand_range(1, height - h - 1);
 
         SDL_Rect new_room = { .x = x, .y = y, .w = w, .h = h };
 
         // If the room overlaps with a previous room, don't create it
         bool overlaps_previous = false;
-        for(int other_r = 0; other_r < room_count; other_r++){
+        for(int other_r = 0; other_r < data->room_count; other_r++){
 
-            if(rect_intersects(&new_room, &rooms[other_r])){
+            if(rect_intersects(&new_room, &(data->rooms[other_r]))){
 
                 overlaps_previous = true;
                 break;
@@ -79,27 +71,26 @@ Vector** map_generate_tiles(int width, int height, MapParams params, bool* succe
         }
 
         // Add the room to the map
-        rooms[room_count] = new_room;
-        create_room(tiles, &new_room);
-        room_count++;
+        int room_index = data->room_count;
+        data->rooms[room_index] = new_room;
+        data->room_count++;
 
         // If the room is the first room we've created, don't do anything further
-        if(room_count == 1){
+        if(room_index == 0){
 
             continue;
         }
 
         // Otherwise, create a tunnel connecting the new room to the previous room
-        int room_index = room_count - 1;
-        int previous_room = room_count - 2;
+        int previous_room = room_index - 1;
 
         Vector new_center = (Vector){
-            .x = rooms[room_index].x + (rooms[room_index].w / 2),
-            .y = rooms[room_index].y + (rooms[room_index].h / 2)
+            .x = data->rooms[room_index].x + (data->rooms[room_index].w / 2),
+            .y = data->rooms[room_index].y + (data->rooms[room_index].h / 2)
         };
         Vector previous_center = (Vector){
-            .x = rooms[previous_room].x + (rooms[previous_room].w / 2),
-            .y = rooms[previous_room].y + (rooms[previous_room].h / 2)
+            .x = data->rooms[previous_room].x + (data->rooms[previous_room].w / 2),
+            .y = data->rooms[previous_room].y + (data->rooms[previous_room].h / 2)
         };
 
         int min_x = min(new_center.x, previous_center.x);
@@ -110,41 +101,126 @@ Vector** map_generate_tiles(int width, int height, MapParams params, bool* succe
         bool tunnel_horizontally = rand() % 2 == 0;
         if(tunnel_horizontally){
 
-            create_room(tiles, &(SDL_Rect){
+            data->hallways[room_index] = (SDL_Rect){
 
                 .x = min_x,
                 .y = previous_center.y,
                 .w = max_x - min_x + 1,
                 .h = 1
-            });
-            create_room(tiles, &(SDL_Rect){
+            };
+            data->hallways[room_index + 1] = (SDL_Rect){
 
                 .x = new_center.x,
                 .y = min_y,
                 .w = 1,
                 .h = max_y - min_y + 1
-            });
+            };
 
         }else{
 
-            create_room(tiles, &(SDL_Rect){
+            data->hallways[room_index] = (SDL_Rect){
 
                 .x = previous_center.x,
                 .y = min_y,
                 .w = 1,
                 .h = max_y - min_y + 1
-            });
-            create_room(tiles, &(SDL_Rect){
+            };
+            data->rooms[room_index + 1] = (SDL_Rect){
 
                 .x = min_x,
                 .y = new_center.y,
                 .w = max_x - min_x + 1,
                 .h = 1
-            });
+            };
         }
     }
 
-    *success = room_count < params.min_rooms;
+    return data;
+}
+
+void map_room_data_free(RoomData* data){
+
+    free(data->rooms);
+    free(data->hallways);
+    free(data);
+}
+
+bool map_room_data_validate(RoomData* data, MapParams params){
+
+    return data->room_count >= params.min_rooms && data->room_count <= params.max_rooms;
+}
+
+Vector** map_generate_tiles(int width, int height, RoomData* data){
+
+    // Start with an empty floors array and an empty tile array
+    bool floors[width][height];
+    Vector** tiles = malloc(sizeof(Vector*) * width);
+    for(int x = 0; x < width; x++){
+
+        tiles[x] = malloc(sizeof(Vector) * height);
+        for(int y = 0; y < height; y++){
+
+            floors[x][y] = false;
+            tiles[x][y] = SPRITE_NONE;
+        }
+    }
+
+    // Fill a 'true' value on the floors for each point on each room and hallway
+    int num_rooms = data->room_count;
+    int num_hallways = (data->room_count - 1) * 2;
+    for(int r = 0; r < num_rooms + num_hallways; r++){
+
+        SDL_Rect to_map;
+        if(r < num_rooms){
+
+            to_map = data->rooms[r];
+
+        }else{
+
+            to_map = data->hallways[r - num_rooms];
+        }
+        for(int x = 0; x < to_map.w; x++){
+
+            for(int y = 0; y < to_map.h; y++){
+
+                floors[x][y] = true;
+            }
+        }
+    }
+
+    // Fill a wall tile on every open space around the floors
+    for(int x = 0; x < width; x++){
+
+        for(int y = 0; y < height; y++){
+
+            if(!floors[x][y]){
+
+                continue;
+            }
+
+            Vector adjacent_squares[4] = {
+                (Vector){ .x = x - 1, .y = y },
+                (Vector){ .x = x + 1, .y = y },
+                (Vector){ .x = x, .y = y - 1 },
+                (Vector){ .x = x, .y = y + 1 }
+            };
+
+            for(int a = 0; a < 4; a++){
+
+                // This shouldn't be possible but it doesn't hurt to check
+                Vector square = adjacent_squares[a];
+                if(square.x < 0 || square.x >= width || square.y < 0 || square.y >= height){
+
+                    continue;
+                }
+
+                if(!floors[square.x][square.y] && tiles[square.x][square.y].x == -1){
+
+                    tiles[square.x][square.y] = SPRITE_TILE_WALL;
+                }
+            }
+        }
+    }
 
     return tiles;
 }
--- src/map.h
+++ src/map.h
@@ -2,12 +2,6 @@
 
 #include "global.h"
 
-typedef struct Map{
-    Vector** tiles;
-    int width;
-    int height;
-} Map;
-
 typedef struct MapParams{
     int max_rooms;
     int min_rooms;
@@ -15,6 +9,22 @@ typedef struct MapParams{
     int room_max_size;
 } MapParams;
 
+typedef struct Map{
+    Vector** tiles;
+    int width;
+    int height;
+} Map;
+
+typedef struct RoomData{
+    SDL_Rect* rooms;
+    SDL_Rect* hallways;
+    int room_count;
+} RoomData;
+
 Map* map_init(int width, int height, MapParams params);
 void map_free(Map* map);
-Vector** map_generate_tiles(int width, int height, MapParams params, bool* success);
+Vector** map_generate_tiles(int width, int height, RoomData* data);
+
+RoomData* map_room_data_generate(int width, int height, MapParams params);
+void map_room_data_free(RoomData* data);
+bool map_room_data_validate(RoomData* data, MapParams params);
